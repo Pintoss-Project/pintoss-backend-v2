00622621f9f681550259b58a11d4828fd8012bf1
wooyong99 <wulovesk@naver.com>
2025-01-05 21:31:09 +0900

    feat : 로그인 컨트롤러 계층 구현

---
+++ src/main/java/com/pintoss/gitftmall/domain/membership/controller/request/LoginRequest.java
@@ -0,0 +1,22 @@
+package com.pintoss.gitftmall.domain.membership.controller.request;
+
+import com.pintoss.gitftmall.common.dto.SelfValidating;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "이메일은 필수 항목입니다.")
+    @Email(message = "이메일 형식에 맞게 입력해주세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 항목입니다.")
+    private String password;
+
+}
