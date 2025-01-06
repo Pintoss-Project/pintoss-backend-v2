0e403f5541380a857e306149a1edd92b544b0a9c
r2dev12 <r2dev12>
2025-01-06 18:19:52 +0900

    refactor : User 엔티티 email, password 제약조건 추가

--- src/main/java/com/pintoss/gitftmall/domain/membership/model/User.java
+++ src/main/java/com/pintoss/gitftmall/domain/membership/model/User.java
@@ -1,16 +1,11 @@
 package com.pintoss.gitftmall.domain.membership.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Generated;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -22,8 +17,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false)
     private String password;
 
     private String name;
