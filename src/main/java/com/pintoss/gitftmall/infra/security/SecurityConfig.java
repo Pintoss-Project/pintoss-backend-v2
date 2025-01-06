8922e854988acdb6c505769c1c64f73fccac0e31
Sangahn <sangahn0524@gmail.com>
2025-01-07 07:43:02 +0900

    feat: SecurityConfig 설정 및 카카오 oauth2 정보 등록

--- src/main/java/com/pintoss/gitftmall/infra/security/SecurityConfig.java
+++ src/main/java/com/pintoss/gitftmall/infra/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.pintoss.gitftmall.infra.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -41,6 +44,7 @@ public class SecurityConfig {
                 request.requestMatchers("/**").permitAll()
                     .anyRequest().authenticated()
             )
+            .oauth2Login(Customizer.withDefaults())
             .build();
     }
 }
