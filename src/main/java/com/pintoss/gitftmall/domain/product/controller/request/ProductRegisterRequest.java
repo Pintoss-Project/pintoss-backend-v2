1b16366224ad500dbc0d5fe3d045a1035871634d
Sangahn <sangahn0524@gmail.com>
2025-01-19 18:56:11 +0900

    refactor: ProductRegisterRequest 요청 스펙에 맞게 수정

--- src/main/java/com/pintoss/gitftmall/domain/product/controller/request/ProductRegisterRequest.java
+++ src/main/java/com/pintoss/gitftmall/domain/product/controller/request/ProductRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.pintoss.gitftmall.domain.product.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -16,20 +17,25 @@ public class ProductRegisterRequest {
     private String name;
 
     @NotNull(message = "인기 여부는 필수 항목입니다.")
+    @JsonProperty("isPopular")
     private boolean isPopular = false;
 
     @NotNull(message = "카드 할인 금액은 필수 항목입니다.")
     @Min(value = 0, message = "카드 할인 금액은 0 이상이어야 합니다.")
+    @JsonProperty("card_discount")
     private BigDecimal cardDiscount = BigDecimal.ZERO;
 
     @NotNull(message = "전화 할인 금액은 필수 항목입니다.")
     @Min(value = 0, message = "전화 할인 금액은 0 이상이어야 합니다.")
+    @JsonProperty("phone_discount")
     private BigDecimal phoneDiscount = BigDecimal.ZERO;
 
     @NotBlank(message = "홈페이지 주소는 필수 항목입니다.")
+    @JsonProperty("home_page")
     private String homePage;
 
     @NotBlank(message = "고객 센터 정보는 필수 항목입니다.")
+    @JsonProperty("cs_center")
     private String csCenter;
 
     @NotBlank(message = "설명은 필수 항목입니다.")
@@ -43,6 +49,7 @@ public class ProductRegisterRequest {
 //    private ProductCategory category;
 
     @NotBlank(message = "로고 이미지는 필수 항목입니다.")
+    @JsonProperty("image_url")
     private String logoImageUrl;
 
     @NotBlank(message = "상품 유의사항은 필수 항목입니다.")
