52f6e3aa7202ddbb908a064d1844f3452aafc108
Sangahn <sangahn0524@gmail.com>
2025-01-19 19:05:57 +0900

    refactor: RequestMapping으로 엔드포인트 등록

--- src/main/java/com/pintoss/gitftmall/domain/product/controller/ProductController.java
+++ src/main/java/com/pintoss/gitftmall/domain/product/controller/ProductController.java
@@ -10,12 +10,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@RestController("/api/product")
+@RestController
+@RequestMapping("/api/product")
 @AllArgsConstructor
 public class ProductController {
 
