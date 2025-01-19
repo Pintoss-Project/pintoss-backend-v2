1cffae873d717d039d776898b41308a1321a3c3a
Sangahn <sangahn0524@gmail.com>
2025-01-19 11:32:57 +0900

    feat: 전체 상품권 조회 기능

--- src/main/java/com/pintoss/gitftmall/domain/product/repository/IProductRepository.java
+++ src/main/java/com/pintoss/gitftmall/domain/product/repository/IProductRepository.java
@@ -1,10 +1,14 @@
 package com.pintoss.gitftmall.domain.product.repository;
 
 import com.pintoss.gitftmall.domain.product.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface IProductRepository {
 
     void save(Product product);
 
     boolean existsByName(String name);
+
+    Page<Product> getProducts(Pageable pageable);
 }
