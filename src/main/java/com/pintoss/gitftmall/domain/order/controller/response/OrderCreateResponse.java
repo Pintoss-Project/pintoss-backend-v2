978707fad113b7be38616c938dac45ad1fd677b6
wooyong99 <wulovesk@naver.com>
2025-02-25 21:39:43 +0900

    refactor : 주문/결제 로직 수정

--- src/main/java/com/pintoss/gitftmall/domain/order/controller/response/OrderCreateResponse.java
+++ src/main/java/com/pintoss/gitftmall/domain/order/controller/response/OrderCreateResponse.java
@@ -1,11 +1,19 @@
 package com.pintoss.gitftmall.domain.order.controller.response;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class OrderCreateResponse {
-
     private Long orderId;
+
+    private Long price;
+
+    private String ordererName;
+
+    private String voucherProvider;
+
+    private LocalDateTime orderDate;
 }
--- src/main/java/com/pintoss/gitftmall/domain/payment/controller/PaymentController.java
+++ src/main/java/com/pintoss/gitftmall/domain/payment/controller/PaymentController.java
@@ -1,11 +1,18 @@
 package com.pintoss.gitftmall.domain.payment.controller;
 
 
+import com.pintoss.gitftmall.core.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/payments")
 @RequiredArgsConstructor
 public class PaymentController {
+
+    @GetMapping("/callback")
+    public ApiResponse<String> paymentCallback() {
+        return ApiResponse.ok("success");
+    }
 
 }
