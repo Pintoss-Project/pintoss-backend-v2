822ac09f44878c1c012097d67625cb2cc5f6997f
r2dev12 <r2dev12>
2025-02-26 18:02:07 +0900

    refactor : 테스트 회원 생성 코드 추가

--- src/test/java/com/pintoss/gitftmall/GitftmallApplicationTests.java
+++ src/test/java/com/pintoss/gitftmall/GitftmallApplicationTests.java
@@ -1,13 +1,36 @@
 package com.pintoss.gitftmall;
 
+import com.pintoss.gitftmall.domain.membership.domain.User;
+import com.pintoss.gitftmall.domain.membership.domain.repository.UserRepository;
+import com.pintoss.gitftmall.domain.membership.domain.vo.Email;
+import com.pintoss.gitftmall.domain.membership.domain.vo.Phone;
+import com.pintoss.gitftmall.domain.membership.domain.vo.RoleEnum;
+import com.pintoss.gitftmall.domain.membership.domain.vo.UserRole;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @SpringBootTest
 class GitftmallApplicationTests {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void createUser() {
+        Email email = new Email("test@naver.com");
+        User user = User.create(email, "test1234!","테스트유저",new Phone("010-1111-1111"), Set.of(new UserRole(RoleEnum.USER)) ,encoder);
+        userRepository.save(user);
     }
 
 }
