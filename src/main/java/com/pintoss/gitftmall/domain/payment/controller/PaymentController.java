345158821a3a9bac1c8cb6c5c784a16c3ed9f0db
r2dev12 <r2dev12>
2025-03-11 17:47:06 +0900

    feat : 결제 redirect 구현

--- src/main/java/com/pintoss/gitftmall/domain/payment/controller/PaymentController.java
+++ src/main/java/com/pintoss/gitftmall/domain/payment/controller/PaymentController.java
@@ -5,8 +5,14 @@ import com.pintoss.gitftmall.core.dto.ApiResponse;
 import com.pintoss.gitftmall.domain.payment.application.PaymentAuthCallbackService;
 import com.pintoss.gitftmall.domain.payment.application.dto.PaymentAuthCallbackServiceCommand;
 import com.pintoss.gitftmall.domain.payment.controller.dto.PaymentAuthCallbackRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/payments")
@@ -16,10 +22,12 @@ public class PaymentController {
     private final PaymentAuthCallbackService paymentAuthCallbackService;
 
     @PostMapping("/callback")
-    public ApiResponse<String> paymentCallback(@ModelAttribute PaymentAuthCallbackRequest request) {
+    public ApiResponse<String> paymentCallback(@ModelAttribute PaymentAuthCallbackRequest request, HttpServletResponse response) throws IOException {
         PaymentAuthCallbackServiceCommand command = PaymentAuthCallbackServiceCommand.from(request);
         // TODO : 사진과 같이 요청을 받는데
         paymentAuthCallbackService.auth(command);
+
+        response.sendRedirect("https://pin-toss.com/order/list");
 
         return ApiResponse.ok(command.getOrderId());
     }
